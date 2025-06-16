68f3b99dab7f6aadedf5e7ad08ea041431960f86
AdamGrzybkowski <agrzybkowski@outlook.com>
2025-06-16 11:22:48 +0200

    Test

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,33 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+plugins {
+    `kotlin-dsl`
+}
+
+group = "com.gravatar.app.buildlogic"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_17
+    }
+}
+
+dependencies {
+
+}
+
+tasks {
+    validatePlugins {
+        enableStricterValidation = true
+        failOnWarning = true
+    }
+}
+
+gradlePlugin {
+    plugins {
+
+    }
+}
