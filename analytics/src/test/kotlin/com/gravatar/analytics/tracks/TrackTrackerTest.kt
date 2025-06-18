b4e3303f47ea5971988d6a7cd4607fdf4eba7a40
Héctor Abraham <hector.abraham.morillo.prieto@automattic.com>
2025-06-18 11:32:50 +0200

    Extract common code in test to a @Before method

--- analytics/src/test/kotlin/com/gravatar/analytics/tracks/TrackTrackerTest.kt
+++ analytics/src/test/kotlin/com/gravatar/analytics/tracks/TrackTrackerTest.kt
@@ -4,14 +4,22 @@ import com.automattic.android.tracks.TracksClient
 import com.gravatar.analytics.Event
 import io.mockk.mockk
 import io.mockk.verify
+import org.junit.Before
 import org.junit.Test
 
 class TrackTrackerTest {
 
+    private lateinit var tracker: TracksTracker
+    private lateinit var mockClient: TracksClient
+
+    @Before
+    fun setUp() {
+        mockClient = mockk<TracksClient>(relaxed = true)
+        tracker = TracksTracker(mockClient)
+    }
+
     @Test
     fun `when trackEvent is invoked then call track on client`() {
-        val mockClient = mockk<TracksClient>(relaxed = true)
-        val tracker = TracksTracker(mockClient)
         tracker.userId = "test_user_id"
         val event = object : Event {
             override val name: String = "test_event"
@@ -24,9 +32,6 @@ class TrackTrackerTest {
 
     @Test
     fun `when flush is invoked then call flush on client`() {
-        val mockClient = mockk<TracksClient>(relaxed = true)
-        val tracker = TracksTracker(mockClient)
-
         tracker.flush()
 
         verify(exactly = 1) { mockClient.flush() }
