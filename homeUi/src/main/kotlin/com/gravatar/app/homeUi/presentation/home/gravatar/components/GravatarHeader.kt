b815c426d2807ca5ff1b6e3eed12a6e9afac94ff
Héctor Abraham <hector.abraham.morillo.prieto@automattic.com>
2025-07-03 16:33:45 +0200

    Reduce more options button alpha

--- homeUi/src/main/kotlin/com/gravatar/app/homeUi/presentation/home/gravatar/components/GravatarHeader.kt
+++ homeUi/src/main/kotlin/com/gravatar/app/homeUi/presentation/home/gravatar/components/GravatarHeader.kt
@@ -1,5 +1,6 @@
 package com.gravatar.app.homeUi.presentation.home.gravatar.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
@@ -11,7 +12,6 @@ import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -21,6 +21,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.BlurredEdgeTreatment
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
@@ -55,6 +56,7 @@ fun GravatarHeader(
             modifier = Modifier
                 .matchParentSize()
                 .blur(radius = 40.dp, edgeTreatment = BlurredEdgeTreatment.Rectangle)
+                .alpha(0.7f)
         )
         Row(
             Modifier
@@ -82,9 +84,8 @@ fun GravatarHeader(
                 onClick = {},
                 modifier = Modifier.align(Alignment.CenterVertically)
             ) {
-                Icon(
+                Image(
                     painter = painterResource(id = R.drawable.more_button),
-                    tint = Color.White,
                     contentDescription = stringResource(R.string.gravatar_tab_header_more_options)
                 )
             }
