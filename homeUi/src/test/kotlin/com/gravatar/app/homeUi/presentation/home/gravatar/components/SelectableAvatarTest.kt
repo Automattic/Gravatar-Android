8bcfe090484c1c812e4ee2dfb83a173e6aae6ff5
Adam Grzybkowski <agrzybkowski@outlook.com>
2025-07-17 16:56:30 +0200

    Update the styling of the selected avatar checkmark (#59)

--- homeUi/src/test/kotlin/com/gravatar/app/homeUi/presentation/home/gravatar/components/SelectableAvatarTest.kt
+++ homeUi/src/test/kotlin/com/gravatar/app/homeUi/presentation/home/gravatar/components/SelectableAvatarTest.kt
@@ -3,6 +3,7 @@ package com.gravatar.app.homeUi.presentation.home.gravatar.components
 import android.net.Uri
 import androidx.compose.foundation.layout.size
 import androidx.compose.ui.Modifier
+import com.gravatar.app.design.theme.GravatarAppTheme
 import com.gravatar.app.homeUi.presentation.home.gravatar.AvatarUi
 import com.gravatar.app.testUtils.roborazzi.RoborazziTest
 import com.gravatar.restapi.models.Avatar
@@ -21,56 +22,64 @@ class SelectableAvatarTest : RoborazziTest() {
 
     @Test
     fun selectableAvatarSelected() = screenshotTest {
-        SelectableAvatar(
-            avatar = AvatarUi.Uploaded(
-                isSelected = true,
-                isLoading = false,
-                avatar = avatar
-            ),
-            size = avatarSize,
-            onAvatarOptionClicked = { _, _ -> },
-            modifier = Modifier.size(avatarSize),
-        )
+        GravatarAppTheme {
+            SelectableAvatar(
+                avatar = AvatarUi.Uploaded(
+                    isSelected = true,
+                    isLoading = false,
+                    avatar = avatar
+                ),
+                size = avatarSize,
+                onAvatarOptionClicked = { _, _ -> },
+                modifier = Modifier.size(avatarSize),
+            )
+        }
     }
 
     @Test
     fun selectableAvatarLoading() = screenshotTest {
-        SelectableAvatar(
-            avatar = AvatarUi.Uploaded(
-                isSelected = true,
-                isLoading = true,
-                avatar = avatar
-            ),
-            size = avatarSize,
-            onAvatarOptionClicked = { _, _ -> },
-            modifier = Modifier.size(avatarSize),
-        )
+        GravatarAppTheme {
+            SelectableAvatar(
+                avatar = AvatarUi.Uploaded(
+                    isSelected = true,
+                    isLoading = true,
+                    avatar = avatar
+                ),
+                size = avatarSize,
+                onAvatarOptionClicked = { _, _ -> },
+                modifier = Modifier.size(avatarSize),
+            )
+        }
     }
 
     @Test
     fun selectableAvatarLoaded() = screenshotTest {
-        SelectableAvatar(
-            avatar = AvatarUi.Uploaded(
-                isSelected = false,
-                isLoading = false,
-                avatar = avatar
-            ),
-            size = avatarSize,
-            onAvatarOptionClicked = { _, _ -> },
-            modifier = Modifier.size(avatarSize),
-        )
+        GravatarAppTheme {
+            SelectableAvatar(
+                avatar = AvatarUi.Uploaded(
+                    isSelected = false,
+                    isLoading = false,
+                    avatar = avatar
+                ),
+                size = avatarSize,
+                onAvatarOptionClicked = { _, _ -> },
+                modifier = Modifier.size(avatarSize),
+            )
+        }
     }
 
     @Test
     fun selectableAvatarFailure() = screenshotTest {
-        SelectableAvatar(
-            avatar = AvatarUi.Local(
-                isLoading = false,
-                uri = Uri.EMPTY
-            ),
-            size = avatarSize,
-            onAvatarOptionClicked = { _, _ -> },
-            modifier = Modifier.size(avatarSize),
-        )
+        GravatarAppTheme {
+            SelectableAvatar(
+                avatar = AvatarUi.Local(
+                    isLoading = false,
+                    uri = Uri.EMPTY
+                ),
+                size = avatarSize,
+                onAvatarOptionClicked = { _, _ -> },
+                modifier = Modifier.size(avatarSize),
+            )
+        }
     }
 }
