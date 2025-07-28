47bffe244497abd6811e02c5b9e09cfc6f7a5d42
Héctor Abraham <hector.abraham.morillo.prieto@automattic.com>
2025-07-28 14:37:28 +0200

    Update ItemDivider color alpha

--- homeUi/src/main/kotlin/com/gravatar/app/homeUi/presentation/home/share/components/ItemDivider.kt
+++ homeUi/src/main/kotlin/com/gravatar/app/homeUi/presentation/home/share/components/ItemDivider.kt
@@ -3,6 +3,7 @@ package com.gravatar.app.homeUi.presentation.home.share.components
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.material3.DividerDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -12,7 +13,7 @@ import androidx.compose.ui.unit.dp
 internal fun ItemDivider(modifier: Modifier = Modifier) {
     Column(modifier) {
         Spacer(modifier = Modifier.height(8.dp))
-        HorizontalDivider(thickness = 1.dp)
+        HorizontalDivider(thickness = 1.dp, color = DividerDefaults.color.copy(alpha = 0.3f))
         Spacer(modifier = Modifier.height(8.dp))
     }
 }
