670b532b3e8e6edc3562e64d89bb29b742d8073f
etoledom <eduardo.toledo@automattic.com>
2025-07-31 10:23:03 +0200

    Refresh avatar on fetchAvatars for all cases
    Co-authored-by: Héctor Abraham Morillo Prieto <hector.abraham.morillo.prieto@automattic.com>

--- userComponent/src/main/kotlin/com/gravatar/app/usercomponent/domain/usecase/FetchAvatarsUseCase.kt
+++ userComponent/src/main/kotlin/com/gravatar/app/usercomponent/domain/usecase/FetchAvatarsUseCase.kt
@@ -7,21 +7,20 @@ import com.gravatar.restapi.models.Avatar
 internal class FetchAvatarsUseCase(
     private val userRepository: UserRepository,
     private val avatarCacheBusterStorage: AvatarCacheBusterStorage,
+    private val clock: AppClock,
 ) : FetchUserAvatars {
 
-    override suspend fun invoke(isRefreshing: Boolean): Result<List<Avatar>> {
+    override suspend fun invoke(): Result<List<Avatar>> {
         return userRepository.getAvatars()
             .onSuccess { avatars ->
-                if (isRefreshing) {
-                    val avatarID = avatars.firstOrNull { it.selected == true }?.imageId
-                    avatarID?.let { id ->
-                        avatarCacheBusterStorage.saveAvatarCacheBuster(id)
-                    }
-                }
+                val selectedAvatarId = avatars.firstOrNull { it.selected == true }?.imageId
+                avatarCacheBusterStorage.saveAvatarCacheBuster(
+                    selectedAvatarId ?: clock.now().toString()
+                )
             }
     }
 }
 
 interface FetchUserAvatars {
-    suspend operator fun invoke(isRefreshing: Boolean): Result<List<Avatar>>
+    suspend operator fun invoke(): Result<List<Avatar>>
 }
