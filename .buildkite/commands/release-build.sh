fb5bbcfe454440854266886cabfed79c520640af
Ian Maia <ian.maia@automattic.com>
2025-08-05 23:31:10 +0200

    Upload to internal track with full rollout

--- .buildkite/commands/release-build.sh
+++ .buildkite/commands/release-build.sh
@@ -7,4 +7,4 @@ echo "--- :closed_lock_with_key: Installing Secrets"
 bundle exec fastlane run configure_apply
 
 echo "--- :hammer_and_wrench: Building and Uploading to Play Store"
-bundle exec fastlane build_and_upload_to_play_store
+bundle exec fastlane build_and_upload_to_play_store track:'internal' release_status:'completed'
