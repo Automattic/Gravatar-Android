f672f1836269f864d15401e4d160501e1c907064
Wojtek Zieba <wojtek.zieba@automattic.com>
2025-08-12 15:40:24 +0200

    Temp: send Sentry issue on the app start

--- app/src/main/java/com/gravatar/app/MainActivity.kt
+++ app/src/main/java/com/gravatar/app/MainActivity.kt
@@ -4,14 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.automattic.android.tracks.crashlogging.CrashLogging
 import com.gravatar.app.design.theme.GravatarAppTheme
 import com.gravatar.app.navigation.RootNavigation
+import org.koin.android.ext.android.get
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val crashLogging: CrashLogging = get()
+        crashLogging.sendReport(exception = Exception("Dev test"), message = "This is just a test")
 
         setContent {
             GravatarAppTheme {
